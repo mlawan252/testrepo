102c4580c73462dd33482557412c464f68d282d5
mlawan252 <77100684+mlawan252@users.noreply.github.com>
2021-09-05 11:38:02 +0100

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,8 @@
 #Display the output
 print("New python File")
+# program that print out the factors of a given integer
+
+number = int(input("Enter a positive integer  "))
+for i in range(1,number + 1):
+             if number % i == 0:
+                 print(f"{i} is a factor of {number}")
